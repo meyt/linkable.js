ee7478ac6c7bb71c421b446dbf9f1963bbc60cc4
dobby <pasd3000@gmail.com>
2019-01-01 19:24:14 +0330

    Add package bundler for demo page

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,31 @@
 import minify from 'rollup-plugin-babel-minify'
 
+const globals = {
+  'emoji-regex/text.js': 'emojiRegex'
+}
+
+const external = ['emoji-regex/text.js']
+
 export default [
   {
     input: './src/index.js',
     output: {
       file: './dist/linkable.js',
       format: 'umd',
-      name: 'Linkable'
-    }
+      name: 'Linkable',
+      globals: globals
+    },
+    external: external
   },
   {
     input: './src/index.js',
     output: {
       file: './dist/linkable.min.js',
       format: 'umd',
-      name: 'Linkable'
+      name: 'Linkable',
+      globals: globals
     },
+    external: external,
     plugins: [
       minify({
         comments: false
